a78779a30e866127e640b85a8204f6e0c119a6c8
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-07-06 19:41:37 -0700

    Updated theme toggle function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+function toggleTheme() {
+    $('.brand-logo').toggleClass('d-none');
+    $('.theme-toggle').toggleClass('d-none');
+    $(document.documentElement).attr('data-bs-theme', function(index, theme) {
+        return theme == 'light' ? 'dark' : 'light'
+    });
+}
