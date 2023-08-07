0c635b2be9f36c361bf8c83018ef2dfaa2286c28
Jonathan Giang <121644251+jagged-dev@users.noreply.github.com>
2023-08-06 20:50:05 -0700

    Test status bar color

--- js/main.js
+++ js/main.js
@@ -8,32 +8,26 @@ $(window).on("beforeunload", function () {
 
 /** theme functions **/
 
-var themeColor;
-
 $(document).ready(function () {
     if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
         document.documentElement.classList.add("dark");
-        themeColor = "dark";
+        $("meta[name='theme-color']").attr("content", "#1d1d1f");
     } else {
         document.documentElement.classList.remove("dark");
-        themeColor = "light";
+        $("meta[name='theme-color']").attr("content", "#f5f5f7");
     }
 });
 
 function toggleTheme() {
     $(document.documentElement).toggleClass("dark");
     localStorage.theme = $(document.documentElement).hasClass("dark") ? "dark" : "light";
+    $("meta[name='theme-color']").attr("content", $(document.documentElement).hasClass("dark") ? "#1d1d1f" : "#f5f5f7");
 }
 
 /** navbar functions **/
 
 $(window).scroll(function () {
     $("nav").attr("data-scrolled", $(this).scrollTop() > 0);
-    if (themeColor === "dark") {
-        $("meta[name='theme-color']").attr("content", $(this).scrollTop() > 0 ? "#1d1d1f" : "#000000");
-    } else if (themeColor === "light") {
-        $("meta[name='theme-color']").attr("content", $(this).scrollTop() > 0 ? "#f5f5f7" : "#ffffff");
-    }
 });
 
 $(window).on("resize", function () {
